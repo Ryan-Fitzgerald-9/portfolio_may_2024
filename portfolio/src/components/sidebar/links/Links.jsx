4f7a45143ab6cefa389244bd95cdcbddebef822a
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2024-04-30 13:22:40 -0400

    sidebar updated

--- portfolio/src/components/sidebar/links/Links.jsx
+++ portfolio/src/components/sidebar/links/Links.jsx
@@ -1,14 +1,47 @@
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.1,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
+
 const Links = () => {
   const items = ["Home", "Skills", "Portfolio", "Contact"];
 
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
       {items.map((item) => (
-        <a href={`#${item}`} key={item}>
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.15 }}
+          whileTap={{ scale: 0.95 }}
+        >
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
