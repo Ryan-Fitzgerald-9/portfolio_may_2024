a02dc6c1638668621f1e9ffaf23987d6e033f9bd
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2024-04-30 18:02:14 -0400

    parallax effect updated

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,17 @@
 import './app.scss'
+import Home from './components/home/Home'
 import Navbar from './components/navbar/Navbar'
+import Parallax from './components/parallax/Parallax'
 
 function App() {
   return <div>
     <section id="Home">
       <Navbar/>
+      <Home/>
     </section>
-    <section id="Skills">Parallax</section>
+    <section id="Skills"><Parallax type="skills"/></section>
     <section>Skills</section>
-    <section id="Portfolio">Parallax</section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
---
+++ portfolio/src/components/home/Home.jsx
@@ -0,0 +1,85 @@
+import "./home.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
+const sliderVariants = {
+  initial: {
+    x: "0%",
+  },
+  animate: {
+    x: "-610%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      ease: "linear",
+      duration: 25,
+    },
+  },
+};
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>RYAN FITZGERALD</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Software Engineer and Web Developer
+          </motion.h1>
+          <motion.div className="buttons" variants={textVariants}>
+            <motion.button variants={textVariants}>
+              See My Latest Projects
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt="scroll"
+          />
+        </motion.div>
+      </div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Full-Stack Creative Collaboration
+      </motion.div>
+      <div className="imageContainer">
+        {/* <img src="/dev_img.png" alt="dev" /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
